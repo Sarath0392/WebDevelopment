9be6bd5569ea76a8728023bda2ccf0170908c2bf
sarath <sarathkumar.rsk@gmail.com>
2020-02-15 23:13:00 +0530

    Registration page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/',function(req,res){
 	res.render("login");
 });
 
+app.get('/register',function(req,res){
+	res.render("registration");
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine',"ejs");
 app.use(express.static('views'));
@@ -13,6 +17,8 @@ app.use(express.static('views'));
 //Routes
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log('server listening on port 3000');
-	console.log(process.env.PORT);
-	console.log(process.env.IP);
 });
+
+// app.listen(3000, function(){
+// 	console.log('server listening on port 3000');
+// });
